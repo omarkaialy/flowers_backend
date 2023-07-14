4c26321cd30c48d9c943ddbd60596db193c98991
Omar Kaialy <omar12kaialy@gmail.com>
2023-07-14 19:13:35 +0300

    add image to

--- src/api/flower/controllers/flower.js
+++ src/api/flower/controllers/flower.js
@@ -6,4 +6,11 @@
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::flower.flower');
+module.exports = createCoreController('api::flower.flower', ({ strapi }) => ({
+  async find(ctx) {
+
+    ctx.query.populate = { image: { fields: ['url', 'blurhash'] } }
+    const entities = await strapi.entityService.findMany('api::flower.flower', ctx.query);
+    return entities;
+  },
+}));
