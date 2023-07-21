198300262f361bcaf2843ab0510ec8399aa36eb2
Omar Kaialy <omar12kaialy@gmail.com>
2023-07-21 20:32:24 +0300

    link category with type

--- src/api/type/controllers/type.js
+++ src/api/type/controllers/type.js
@@ -6,4 +6,11 @@
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::type.type');
+module.exports = createCoreController('api::type.type', ({ strapi }) => ({
+  async find(ctx) {
+    ctx.query.populate = '*';
+    const types = await strapi.entityService.findMany('api::type.type', ctx.query);
+    return { data: types }
+  }
+
+}));
